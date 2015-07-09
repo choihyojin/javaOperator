e37a4651b3081ed29a188b3b968a2eac48d13f84
평일반 <@ITbank011-PC>
2015-07-09 15:21:09 +0900

    2015.07.09

---
+++ JavaOperator/src/ifCondition/ArithmeticCalc.java
@@ -0,0 +1,59 @@
+package ifCondition;
+
+import java.util.Scanner;
+
+/*
+ * @ Date: 2015.07.09
+ * @ Author : me
+ * @ Story : 사칙연산 계산 프로그램
+ * */
+/*
+ * 사칙연산 계산기
+ * 덧셈,뺄셈,곱셉,나눗셈(몫:() 나머지:())
+ * 사칙연산 기호도 입력받음 + ,- ,*,/
+ * 출력결과
+ * === 심플 계산기 ===
+ * 5 X 5 = 25
+ * 5 + 5 = 10
+ * 6 ÷ 5 = 1 (1)
+ * 5 - 6 = -1
+ * */
+public class ArithmeticCalc {
+
+	public void calc(){
+		Scanner scanner = new Scanner(System.in);
+		
+		int nmg=0,result=0;
+		int su1=0,su2=0;
+		String a="";
+		
+		System.out.println("숫자를 입력하세요");
+		su1=scanner.nextInt();
+		
+		System.out.println("연산자를 입력하세요");
+		a=scanner.next();
+		
+		System.out.println("숫자를 입력하세요");
+		su2=scanner.nextInt();
+		
+		
+		if (a.equals("+")){
+			result=su1+su2;
+		}
+		else if (a.equals("-")){
+			result=su1-su2;
+		}
+		else if (a.equals("*")){
+			result=su1*su2;
+		}
+		else if (a.equals("/")){
+			result=su1/su2;
+			nmg=su1%su2;
+		}
+		else{
+			System.out.println("연산자를 다시 입력하세요.");
+		}
+		System.out.println(su1+a+su2+"="+result+"("+nmg+")");
+
+	}
+}
---
+++ JavaOperator/src/ifCondition/CalcAvg.java
@@ -0,0 +1,49 @@
+package ifCondition;
+
+import java.util.Scanner;
+
+public class CalcAvg {
+	public void calcAvg(){
+		Scanner scanner = new Scanner(System.in);
+		
+		//선언부
+		String name="",msg="";
+		//점수는 연산을 해야되서 int 타입으로 정해져야 한다.
+		int kor=0,eng=0,math=0,total=0,avg=0;
+		
+		
+		System.out.println("이름을 입력하세요");
+		name=scanner.next();
+		
+		System.out.println("국어 점수를 입력하세요");
+		kor=scanner.nextInt();
+		System.out.println("영어 점수를 입력하세요");
+		eng=scanner.nextInt();
+		System.out.println("수학 점수를 입력하세요");
+		math=scanner.nextInt();
+		
+		total=kor+eng+math;
+		avg=(int)total/3;//다운캐스트 소수점 이하 버림
+		
+		
+		//연산부
+		if (avg>=90) {
+			msg="장학생";
+		}
+		else if ((avg>=70)&&(avg<90)) {
+			msg="합격";
+		}
+		else {
+			msg="불합격";
+		}
+		//출력부	
+		System.out.println("학생\t국어\t영어\t수학\t총점\t평균\t합격여부");
+		System.out.println("-------------------------------------------------------------");
+		System.out.println(name+"\t"+kor+"\t"+eng+"\t"+math+"\t"+total+"\t"+avg+"\t"+msg);
+		
+		
+	}
+
+}
+
+
