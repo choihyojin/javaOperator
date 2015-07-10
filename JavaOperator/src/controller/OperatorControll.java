cc648f411de2d22dc7de7956268994f60329d078
평일반 <@ITbank011-PC>
2015-07-10 15:03:40 +0900

    2015.07.10 2

--- JavaOperator/src/controller/OperatorControll.java
+++ JavaOperator/src/controller/OperatorControll.java
@@ -2,6 +2,7 @@ package controller;
 
 import condition.ArithmeticCalc;
 import condition.CalcAvg;
+import condition.ManOrWoman;
 import condition.SwitchAvg;
 
 public class OperatorControll {
@@ -16,7 +17,11 @@ public class OperatorControll {
 		
 		//2015.07.10
 		SwitchAvg avg2 = new SwitchAvg();
-		avg2.calc();
+//		avg2.calc();
+		
+		//2015.07.10 2
+		ManOrWoman manOrWoman = new ManOrWoman();
+		manOrWoman.ssn(); //ssn() 원 클래스의 메소드 이름이다.
 	}
 
 }
