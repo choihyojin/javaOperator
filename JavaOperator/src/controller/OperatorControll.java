d24bc792c31dd89ced08eeb47eda284de83ee775
평일반 <@ITbank011-PC>
2015-07-13 15:17:01 +0900

    2015.07.13

--- JavaOperator/src/controller/OperatorControll.java
+++ JavaOperator/src/controller/OperatorControll.java
@@ -4,6 +4,7 @@ import condition.ArithmeticCalc;
 import condition.CalcAvg;
 import condition.ManOrWoman;
 import condition.SwitchAvg;
+import loop.EvenOddSum;
 
 public class OperatorControll {
 	public static void main(String[] args) {
@@ -21,7 +22,11 @@ public class OperatorControll {
 		
 		//2015.07.10 2
 		ManOrWoman manOrWoman = new ManOrWoman();
-		manOrWoman.ssn(); //ssn() 원 클래스의 메소드 이름이다.
+//		manOrWoman.ssn(); //ssn() 원 클래스의 메소드 이름이다.
+		
+		//2015.07.13
+		EvenOddSum evenOddSum = new EvenOddSum();
+		evenOddSum.calc();
 	}
 
 }
